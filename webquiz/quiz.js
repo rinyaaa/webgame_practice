b3f073659f7d0b78486df85f33cb4409fbeb5dd2
rinyaaa <168408089+rinyaaa@users.noreply.github.com>
2024-10-10 21:02:11 +0900

    完成

--- webquiz/quiz.js
+++ webquiz/quiz.js
@@ -1,52 +1,73 @@
-const question = 'ゲーム市場、もっとも売れたゲームきたどれ？';
-
-const answers = ['スーパファミコン',
-'プレイステーション２',
-'ニンテンドウースイッチ',
-'ニンテンドーDS'
+const quiz = [
+    {
+        question: 'ゲーム市場、もっとも売れたゲームきたどれ？',
+        answers: [
+            'スーパファミコン',
+            'プレイステーション２',
+            'ニンテンドウースイッチ',
+            'ニンテンドーDS'
+        ],
+        correct: 'ニンテンドーDS'
+    }, {
+        question: '糸井重勝何企画に関わった、任天堂のゲームといえば？',
+        answers: [
+            'MOTHER2',
+            'スパーマリオブラザーズ3',
+            'スーパードンキーコング',
+            '星のカービー'
+        ],
+        correct: 'MOTHER2'
+    }, {
+        question: 'ハイキューの主人公の名前は',
+        answers: [
+            'ひなた',
+            'かげやま',
+            'おおいかわ',
+            'うしわか'
+        ],
+        correct: 'ひなた'
+    },
 ];
-const correct = 'ニンテンドーDS';
 
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-const setupQuiz = () =>{
-    document.getElementById('js-question').textContent = question;
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = quiz[quizIndex].question; // 修正
     let buttonIndex = 0;
-    let buttonLength = $button.length
-    
-    while(buttonIndex < buttonLength){
-        $button[buttonIndex].textContent = answers[buttonIndex];
+
+    while (buttonIndex < buttonLength) {
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
-    };
+    }
 };
 setupQuiz();
 
-const clickHandler = (e) =>{
-    if(correct === e.target.textContent ){
+const clickHandler = (e) => {
+    if (quiz[quizIndex].correct === e.target.textContent) {
         window.alert('正解');
-    }else {
+        score++;
+    } else {
         window.alert('間違い');
+    }
+
+    quizIndex++;
+
+    if (quizIndex < quizLength) {
+        // 問題がまだある場合
+        setupQuiz();
+    } else {
+        window.alert("あなたのスコアは"+ score +'です'); // 修正
     }
 };
 
 let handlerIndex = 0;
 
-while(handlerIndex < buttonLength){
+while (handlerIndex < buttonLength) {
     $button[handlerIndex].addEventListener('click', (e) => clickHandler(e));
     handlerIndex++;
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
